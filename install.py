410e52f4937c1cf429f1132bacd077f4e85ae5d1
Tim Calvert <tcalvert93@gmail.com>
2014-10-04 15:29:42 -0400

    model file and updated requirements

---
+++ install.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Time, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+import os
+
+engine = create_engine(os.environ.get("HEROKU_POSTGRESQL_ORANGE_URL", "postgres://pdvxuefuwwxotp:6p_RAHUaJa4ClWP20EGsHbISJe@ec2-54-243-42-236.compute-1.amazonaws.com:5432/d62ri4ks7m4d6"), echo=True)
+
+Base = declarative_base()
+
+class Message(Base):
+	__tablename__ = 'messages'
+
+	#basic fields
+	id = Column(Integer, primary_key=True)
+	latLocation = Column(Float, nullable=False)
+	lonLocation = Column(Float, nullable=False)
+	message = Column(String, nullable=False)
+	timeLogged = Column(DateTime, nullable=False)
+	
+	#timed duration fields
+	expireTime = Column(DateTime)
+	
+	#recurring times
+	beginTime = Column(Time)
+	endTime = Column(Time)
+	daysVisible = Column(Integer)
+	
+	#unlocks
+	requiredMessageId = Column(Integer, ForeignKey("messages.id"))
+	requireMessage = relationship("Message")
+
+	def __repr__(self):
+		return "<Message([%s] at (%f, %f) '%s'>" % (
+			(str(self.id) if self.id else 'None'), self.latLocation, self.lonLocation, self.message)

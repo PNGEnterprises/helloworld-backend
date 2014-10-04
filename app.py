b39bcc05d41f82e41895a1c74e194144d1b1c4a4
Riley Larkins <rdlarkins@wpi.edu>
2014-10-04 16:32:48 -0400

    Added routes

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/api/v1/get-message", methods=['POST'])
+def get_message():
+	return "You got a message"
+
+@app.route("/api/v1/post-message", methods=['POST'])
+def post_message():
+	return "You posted a message"
+
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 5000))
